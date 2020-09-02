aeeec6398b67b25dba29f20f728c8a3cf5856e1d
JAVI <ortiz.javier10@gmail.com>
2020-09-01 20:58:37 -0400

    log in and log out

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,27 @@
-// const { report } = require("../../controllers");
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email, 
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
 
 async function signupFormHandler(event) {
     event.preventDefault();
@@ -27,30 +50,10 @@ async function signupFormHandler(event) {
     }
   }
 
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 
-async function loginFormHandler(event) {
-    event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
 
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email, 
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
 
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
