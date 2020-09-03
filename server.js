1316dad180971908e54570b373c5f41db1e46d4d
JAVI <ortiz.javier10@gmail.com>
2020-09-03 07:16:09 -0400

    final touches

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const sess = {
 app.use(session(sess));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
