8a6268d7a1a2fc83fea85a1b692daacce34a7553
JAVI <ortiz.javier10@gmail.com>
2020-09-06 10:45:28 -0400

    final commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers});
+// const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,6 +22,11 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
